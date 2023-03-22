9e1c12c54fec1113bbf780ff6a5f8a9091679a61
9rum <sanha.maeng@kakao.com>
2023-03-22 16:07:46 +0900

    freq_generator.py: remove redundant computation in index calculation

--- freq_generator.py
+++ freq_generator.py
@@ -7,14 +7,15 @@ from tqdm import tqdm
 def generate_freq():
   for dataset in ["reddit", "ogbn_products", "ogbn_papers100M"]:
     degrees = pd.read_csv(f"logs/degree_{dataset}.csv")["degree"]
-    frequencies = np.zeros(shape=(degrees.max()-degrees.min()+1), dtype=np.int64)
+    min_degree, max_degree = degrees.min(), degrees.max()
+    frequencies = np.zeros(shape=(max_degree-min_degree+1), dtype=np.int64)
 
     for degree in tqdm(degrees):
-      frequencies[degree-degrees.min()] += 1
+      frequencies[degree-min_degree] += 1
 
     df = pd.DataFrame(columns=["degree", "frequency"])
     for degree, freq in enumerate(tqdm(frequencies)):
-      df.loc[len(df)] = [degree+degrees.min(), freq]
+      df.loc[len(df)] = [degree+min_degree, freq]
 
     df.to_csv(f"logs/freq_{dataset}.csv", index=False)
 
