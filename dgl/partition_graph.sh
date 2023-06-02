4f58400112f19e4d1cb023a35e3e0d758214b71a
CharlieKim <skdbsxir@naver.com>
2023-06-03 03:08:11 +0900

    dgl: fix to get NUM_GPUS per machine

--- dgl/partition_graph.sh
+++ dgl/partition_graph.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-python3 partition_graph.py --num_parts $NUM_PARTS --balance_train --balance_edges --output_dir ./dataset/reddit_partition
+python3 partition_graph.py --num_parts $NUM_PARTS --num_trainers_per_machine $NUM_GPUS --balance_train --balance_edges --output_dir ./dataset/reddit_partition
