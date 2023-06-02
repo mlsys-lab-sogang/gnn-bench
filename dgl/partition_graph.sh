7d93154ac6ead56585de16cc4f9f8bb926f00dcf
CharlieKim <skdbsxir@naver.com>
2023-06-02 21:47:17 +0900

    dgl: fix dataset dir Since workdir is 'dgl', fixed dataset directory to 'dgl/dataset/'.

--- dgl/partition_graph.sh
+++ dgl/partition_graph.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-python3 partition_graph.py --num_parts $NUM_PARTS --balance_train --balance_edges --output_dir ../dataset/reddit_partition
+python3 partition_graph.py --num_parts $NUM_PARTS --balance_train --balance_edges --output_dir ./dataset/reddit_partition
